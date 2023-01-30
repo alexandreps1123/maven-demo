75a556cf01c3dfeec334d65ac05af2240e93b6ee
alexandreps1123 <alexandre.ps1123@gmail.com>
2023-01-30 13:47:44 -0300

    implementation servlets

---
+++ userweb/src/main/java/com/parkus/user/servlets/CreateUserServlet.java
@@ -0,0 +1,17 @@
+package com.parkus.user.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CreateUserServlet extends HttpServlet {
+    
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+    }
+}
